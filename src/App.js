0be34964b602f07fc69f9da765390465e2229566
ALLA HEMANTH VENKATA PAVAN KUMAR REDDY <ahvworkatro@gmail.com>
2019-07-07 22:33:50 +0530

    Adding State to components

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    person : [
+      { name : "pavan", age : 23},
+      {name : "ahv", age : 24},
+      {name : "reddy", age:22}
+    ],
+
+    otherState : "otherStateValue"
+
+  };
+
   render() {
     return (
       <div className="App">
         <h1>This is my first react js application</h1>
-        <Person name="pavan" age="23" />
-        <Person name="ahv" age="24">
+        <Person name={this.state.person[0].name} age={this.state.person[0].age} />
+        <Person name={this.state.person[1].name} age={this.state.person[1].age}>
           This is my intial
         </Person>
-        <Person name="reddy" age="22" />
+        <Person name={this.state.person[2].name} age={this.state.person[2].age} />
       </div>
     );
   }
